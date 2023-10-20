d040849567c587c2adab0f28e9673804757b0b52
Redstonik <lkolba2626@gmail.com>
2023-10-20 14:29:51 +0300

    Complited solution architecture, also added user entity, repository, dbContext, and tested it.

---
+++ Lab1.Database/StartupSetup.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Contracts.Data;
+using Lab1.Database.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Database
+{
+    public static class StartupSetup
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+        }
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<Lab1DbContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
---
+++ Lab1.Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab1.Services
+{
+    public class Class1
+    {
+
+    }
+}
